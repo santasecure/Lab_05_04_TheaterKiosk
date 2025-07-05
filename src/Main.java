4f35c849d092d6d470bbe87ac91193de9a52748d
Kristie Santa <k-santa@hotmail.com>
2025-07-05 15:09:43 -0400

    Project TheatreKiosk complete

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Input age, if age >= 21 output wristband message, otherwise do nothing
+
+        // Declare variables
+        int age = 0;
+
+        // Create scanner for input
+        Scanner input = new Scanner(System.in);
+
+        // Get user input
+        System.out.print("Enter your age: ");
+        age = input.nextInt();
+
+        // If condition
+        if (age >= 21) {
+            System.out.println("You get a wristband.");
         }
+
+        input.close();
     }
 }
